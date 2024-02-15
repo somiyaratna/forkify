1559a6b7903c6b39eb5abb27e018b26884b0d303
somiyaratna <beherasomiyaratna@gmail.com>
2024-02-15 14:13:14 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'baeda3d8-8b6a-4b14-88b8-a2a0c348e2ef';
+export const MODAL_CLOSE_SEC = 2.5;
